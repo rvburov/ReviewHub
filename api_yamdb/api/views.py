ea476472846b05cff7171524ab6c309e94f6258f
Roman Burov <romanburov89@gmail.com>
2023-11-20 02:42:26 +0300

    Первое сообщение

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets
+
+from api.permissions import IsAuthorModerAdminOrReadOnly
+from api.serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Review, Title
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorModerAdminOrReadOnly,)
+
+    def get_review(self):
+        return get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthorModerAdminOrReadOnly,)
+
+    def get_title(self):
+        return get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
