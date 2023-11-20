7dc82afd8510f004b4961d6d973deee441d85788
Евгения Литвинова <semi-transparent@ya.ru>
2023-11-20 21:02:51 +0300

    First commit

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,17 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
+import datetime as dt
 
 from reviews.models import Title, Genre, Category
-
-
-class TitleSerializer(serializers.ModelSerializer):
-
-    # TODO написать кастомное поля с валидацией года
-    # TODO написать валидацию однократной связи с жанром и категорией
-    # TODO написать связь с категорией и жаром по слагу
-
-    class Meta:
-        fields = ('id', 'name', 'year', 'genre', 'category', 'description')
-        model = Title
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -26,3 +16,37 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('name', 'slug')
         model = Category
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category', )
+        model = Title
+
+
+class TitleChangeSerializer(serializers.ModelSerializer):
+
+    genre = serializers.SlugRelatedField(slug_field='name',
+                                         queryset=Genre.objects.all(),
+                                         many=True)
+    category = serializers.SlugRelatedField(slug_field='name',
+                                            queryset=Category.objects.all())
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category', )
+        model = Title
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Genre.objects.all(),
+                fields=('title', 'genre'),
+                message='Этот жанр уже добавлен')
+        ]
+
+    def validate_year(self, value):
+        if value < dt.date.today().year:
+            return value
+        raise serializers.ValidationError('Гостей из будущего не ждали')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, mixins
 
 from reviews.models import Title, Genre, Category
-from api.serializers import (TitleSerializer,
+from api.serializers import (TitleChangeSerializer,
+                             TitleReadSerializer,
                              GenreSerializer,
                              CategorySerializer)
 
@@ -9,26 +10,34 @@ from api.serializers import (TitleSerializer,
 class TitleViewSet(viewsets.ModelViewSet):
     '''Viewset для модели Title'''
     queryset = Title.objects.all()
-    serializer_class = TitleSerializer
     permission_classes = ()
     filter_backends = filters.SearchFilter
     filterset_fields = ('name', 'year', 'category', 'genre',)
     http_method_names = ['get', 'post', 'patch', 'delete',]
 
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return TitleReadSerializer
+        else:
+            return TitleChangeSerializer
 
-class GenreViewSet(viewsets.ModelViewSet):
+
+class GenreCategoryViewSet(mixins.ListModelMixin,
+                           mixins.CreateModelMixin,
+                           mixins.DestroyModelMixin,
+                           viewsets.GenericViewSet,):
+    permission_classes = ()
+    filter_backends = filters.SearchFilter
+    filterset_fields = ('name',)
+
+
+class GenreViewSet(GenreCategoryViewSet):
     '''Vieset для модели Genre'''
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = ()
-    filter_backends = filters.SearchFilter
-    filterset_fields = ('name',)
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(GenreCategoryViewSet):
     '''Viewset для модели Category'''
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = ()
-    filter_backends = filters.SearchFilter
-    filterset_fields = ('name',)
